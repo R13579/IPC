8637c7961ef94ebd32f430b4dd396783631baeb4
R13579 <ranjithk24k@gmail.com>
2023-03-15 12:35:10 +0530

    cust_atoi.c added

---
+++ cust_atoi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+long long int atoi(char *s)
+{
+long long int a=0;
+while(*s)
+{
+	a=a*10+*s-'0';
+	s++;
+
+}
+return a;
+}
+int main()
+{
+char s[]="123456789123";
+long long int a=atoi(s);
+
+printf("%lld",a);
+}
